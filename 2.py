7b38ebb6a4e582be0d99a96559829ec043184a4b
Karol <vkarol.kontakt@gmail.com>
2022-08-03 13:32:11 +0200

    Capital Guess

---
+++ 2.py
@@ -0,0 +1,13 @@
+capitals = {
+    'Poland': 'Warszawa',
+    'Francja': 'Paris',
+    'Germany': 'Berlin',
+    'Hungary': 'Budapest',
+    'Austria': 'Vienna',
+}
+
+capital = input('What is the capital of Germany? ')
+if capitals['Germany'] == capital:
+    print('Good job!')
+else:
+    print('The capital of Germany is Berlin. ')
