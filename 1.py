18d2b861344e9d74362a9783309af5a6902e22bb
Karol <vkarol.kontakt@gmail.com>
2022-08-03 13:10:51 +0200

    What is your name?

---
+++ 1.py
@@ -0,0 +1,33 @@
+x = 1
+y = 2
+
+#tuple
+point = 1,2
+print(point[0])
+print(point[1])
+print(point)
+
+#list
+cities = ['Wrocław', 'Warszawa', 'Poznań', 'Sopot']
+print(cities[0])
+print(cities[-1])
+
+cities.append('Kraków')
+cities.append('Katowice')
+cities.append('Kołobrzeg')
+
+print(cities)
+
+cities.sort()
+
+print(cities)
+
+#słownik
+
+capitals = {
+    'Poland': 'Warszawa',
+    'Francja': 'Paris',
+    'Germany': 'Berlin'
+}
+
+print(capitals['Germany'])
